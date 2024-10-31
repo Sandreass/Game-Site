872e10e6d03fc29af021f6c2df158538d3932d94
Andrei Stroianetki <sandreasweb@gmail.com>
2024-10-31 10:34:28 +0100

    update slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import zorro from '/public/zorro.jpg';
 import robin from '/public/robin.jpg';
 import jimbei from '/public/jimbei.jpg';
 
 const Slider = () => {
-  // Используем переменные, а не строки
   const images = [jimbei, zorro, robin];
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
 
   const nextSlide = () => {
     setCurrentIndex((currentIndex + 1) % images.length);
@@ -19,9 +26,9 @@ const Slider = () => {
 
   return (
     <div className="relative w-full h-96 bg-gray-200">
-      <button onClick={prevSlide} className="absolute left-0">Prev</button>
+      <button onClick={prevSlide} className="absolute left-0 p-2 bg-white bg-opacity-50 rounded-full">Prev</button>
       <img src={images[currentIndex]} alt="slide" className="w-full h-full object-cover" />
-      <button onClick={nextSlide} className="absolute right-0">Next</button>
+      <button onClick={nextSlide} className="absolute right-0 p-2 bg-white bg-opacity-50 rounded-full">Next</button>
     </div>
   );
 };
